ccf164ef36c986280cebde8bdc4611a9bb5ba222
Aadi Umrani <aadi.p.um@gmail.com>
2023-03-13 21:41:00 -0400

    .

--- sample.py
+++ sample.py
@@ -2,11 +2,10 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-fileName = input("Please enter the name of the file: ")
-type = input("Please enter the file extension: ")
+fileName = input("Please enter the name of the file with the extension: ")
 
 # Load image (LOAD)
-img = cv2.imread('images/' + fileName + '.' + type)
+img = cv2.imread('images/' + fileName)
 
 # Convert image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -20,10 +19,16 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Detect faces in image (AIM)
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-# Draw rectangles around detected faces
-for (x,y,w,h) in faces:
-    cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 10)
+# If faces are detected, draw rectangles around them and display the number of faces detected
+if len(faces) > 0:
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,0,255), 10)
 
-# Display image with detected faces (SHOOT)
-plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-plt.show()
+    # Display image with detected faces and number of faces detected
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.title(str(len(faces)) + ' face(s) detected')
+    plt.show()
+    
+# If no faces are detected, display a message
+else:
+    print("No faces detected in the image.")
